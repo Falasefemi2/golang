0f1f6858bc19a4d2da375c8a7fbe13fdf39d7326
Falasefemi2 <femifalase228@gmail.com>
2024-04-12 22:56:06 +0100

    append

--- Slices/list.go
+++ Slices/list.go
@@ -3,16 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	var productNames [4]string = [4]string{"A book"}
-	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
-	fmt.Println(prices)
-
-	productNames[2] = "A Carpet"
-
-	fmt.Println(productNames)
-	fmt.Println(prices[2])
-	fmt.Println(productNames)
-
-	featuredPrices := prices[1:3]
-	fmt.Println(featuredPrices)
+	prices := []float64{10.99, 8.99}
+	fmt.Println(prices[0:1])
+	prices[1] = 9.99
+	updatedPrices := append(prices, 5.99)
+	fmt.Println(updatedPrices, prices)
 }
+
+// func main() {
+// 	var productNames [4]string = [4]string{"A book"}
+// 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+// 	fmt.Println(prices)
+
+// 	productNames[2] = "A Carpet"
+
+// 	fmt.Println(productNames)
+// 	fmt.Println(prices[2])
+// 	fmt.Println(productNames)
+
+// 	featuredPrices := prices[1:]
+// 	featuredPrices[0] = 199.99
+// 	highLightedPrices := featuredPrices[:1]
+// 	fmt.Println(highLightedPrices)
+// 	fmt.Println(featuredPrices)
+// 	fmt.Println(len(highLightedPrices), cap(highLightedPrices))
+
+// 	highLightedPrices = highLightedPrices[:3]
+// 	fmt.Println(highLightedPrices)
+// 	fmt.Println(featuredPrices)
+// 	fmt.Println(len(highLightedPrices), cap(highLightedPrices))
+// }
