d34a0c424d2a36cbb2125137c416f177c93253b5
Falasefemi2 <femifalase228@gmail.com>
2024-04-14 21:08:58 +0100

    exercise

--- Slices/list.go
+++ Slices/list.go
@@ -2,34 +2,69 @@ package main
 
 import "fmt"
 
-func main() {
-	prices := []float64{10.99, 8.99}
-	fmt.Println(prices[0:1])
-	prices[1] = 9.99
-	updatedPrices := append(prices, 5.99)
-	fmt.Println(updatedPrices, prices)
+type product struct {
+	id    string
+	title string
+	price int
 }
 
-// func main() {
-// 	var productNames [4]string = [4]string{"A book"}
-// 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
-// 	fmt.Println(prices)
+func main() {
+	// 1
+	hobbies := [3]string{"eating", "cooking", "dancing"}
+	fmt.Println(hobbies)
 
-// 	productNames[2] = "A Carpet"
+	// 2
+	fmt.Println(hobbies[0])
+	fmt.Println(hobbies[1:])
 
-// 	fmt.Println(productNames)
-// 	fmt.Println(prices[2])
-// 	fmt.Println(productNames)
+	// 3
+	mainHobbies := hobbies[:2]
+	fmt.Println(mainHobbies)
 
-// 	featuredPrices := prices[1:]
-// 	featuredPrices[0] = 199.99
-// 	highLightedPrices := featuredPrices[:1]
-// 	fmt.Println(highLightedPrices)
-// 	fmt.Println(featuredPrices)
-// 	fmt.Println(len(highLightedPrices), cap(highLightedPrices))
+	// 4
+	fmt.Println(cap(mainHobbies))
+	mainHobbies = mainHobbies[1:3]
+	fmt.Println(mainHobbies)
 
-// 	highLightedPrices = highLightedPrices[:3]
-// 	fmt.Println(highLightedPrices)
-// 	fmt.Println(featuredPrices)
-// 	fmt.Println(len(highLightedPrices), cap(highLightedPrices))
-// }
+	// 5
+	courseGoals := []string{"be good at golang", "get a job"}
+	fmt.Println(courseGoals)
+
+	// 6
+	courseGoals[1] = "Learn all the details"
+	courseGoals = append(courseGoals, "Know this very well")
+	fmt.Println(courseGoals)
+
+	// 7
+	products := []product{
+		{
+			"id",
+			"A first ",
+			300,
+		},
+		{
+			"ids",
+			"A second product",
+			4000,
+		},
+	}
+	fmt.Println(products)
+}
+
+// Time to practice what you learned!
+
+// 1) Create a new array (!) that contains three hobbies you have
+// 		Output (print) that array in the command line.
+// 2) Also output more data about that array:
+//		- The first element (standalone)
+//		- The second and third element combined as a new list
+// 3) Create a slice based on the first element that contains
+//		the first and second elements.
+//		Create that slice in two different ways (i.e. create two slices in the end)
+// 4) Re-slice the slice from (3) and change it to contain the second
+//		and last element of the original array.
+// 5) Create a "dynamic array" that contains your course goals (at least 2 goals)
+// 6) Set the second goal to a different one AND then add a third goal to that existing dynamic array
+// 7) Bonus: Create a "Product" struct with title, id, price and create a
+//		dynamic list of products (at least 2 products).
+//		Then add a third product to the existing list of products.
