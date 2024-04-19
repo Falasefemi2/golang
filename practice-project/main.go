176962e1be84c35af47a980a1ead6237a4cc7389
Falasefemi2 <femifalase228@gmail.com>
2024-04-19 13:39:09 +0100

    got the calculation done

--- practice-project/main.go
+++ practice-project/main.go
@@ -1,21 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"example.com/project/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 20}
 	taxRate := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRate {
-		var taxIncludedPrices []float64 = make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
-
-	fmt.Println(result)
 
 }
