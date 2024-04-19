65d7788b2052f7e2e4e3cddd9a5b53bad103f733
Falasefemi2 <femifalase228@gmail.com>
2024-04-19 21:02:19 +0100

    brought in interface

--- practice-project/main.go
+++ practice-project/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
-
-	"example.com/project/filemanager"
+	"example.com/project/cmdproject"
 	"example.com/project/prices"
 )
 
@@ -11,8 +9,9 @@ func main() {
 	taxRate := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRate {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		cmdm := cmdproject.New()
+		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
 
--- practice-project/prices/prices.go
+++ practice-project/prices/prices.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 
 	"example.com/project/conversion"
-	"example.com/project/filemanager"
+	"example.com/project/iomanager"
 )
 
 type TaxIncludedPriceJob struct {
-	IOManger          filemanager.FileManager `json:"-"`
-	TaxRate           float64                 `json:"tax_rate"`
-	InputPrices       []float64               `json:"input_prices"`
-	TaxIncludedPrices map[string]string       `json:"tax_included_prices"`
+	IOManger          iomanager.IOManager `json:"-"`
+	TaxRate           float64             `json:"tax_rate"`
+	InputPrices       []float64           `json:"input_prices"`
+	TaxIncludedPrices map[string]string   `json:"tax_included_prices"`
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
@@ -54,9 +54,9 @@ func (job *TaxIncludedPriceJob) Process() {
 	job.IOManger.WriteResult(job)
 }
 
-func NewTaxIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(iom iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
-		IOManger:    fm,
+		IOManger:    iom,
 		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
