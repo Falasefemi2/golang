a60bbe9c8df898cc9224178f0bab59a5231e15d9
Falasefemi2 <femifalase228@gmail.com>
2024-05-02 12:44:48 +0100

    add to database

--- Rest-Api/models/event.go
+++ Rest-Api/models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/restapi/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,9 +17,23 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
+func (e Event) Save() error {
 	// later: add to database
-	events = append(events, e)
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	results, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := results.LastInsertId()
+	e.ID = id
+	return err
 }
 
 func GetAllEvents() []Event {
