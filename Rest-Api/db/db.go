d58d0a48f98fe072ec0ff2f8e2470b042c6ff04c
Falasefemi2 <femifalase228@gmail.com>
2024-05-02 12:37:19 +0100

    rewrote the db.go file

--- Rest-Api/db/db.go
+++ Rest-Api/db/db.go
@@ -1,41 +1,90 @@
+// package db
+
+// import (
+// 	"database/sql"
+
+// 	_ "github.com/mattn/go-sqlite3"
+// )
+
+// var DB *sql.DB
+
+// func InitDB() {
+// 	DB, err := sql.Open("sqlite3", "api.db")
+
+// 	if err != nil {
+// 		panic("Could not connect to database")
+// 	}
+
+// 	DB.SetMaxOpenConns(10)
+// 	DB.SetMaxIdleConns(5)
+
+// 	createTables()
+// }
+
+// func createTables() {
+// 	createEventsTable := `
+// 	CREATE TABLE IF NOT EXISTS events (
+// 		id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         name TEXT NOT NULL,
+// 		description TEXT NOT NULL,
+// 		location TEXT NOT NULL,
+// 		dateTime DATETIME NOT NULL,
+// 		user_id INTEGER
+// 	)
+// 	`
+
+// 	_, err := DB.Exec(createEventsTable)
+
+// 	if err != nil {
+// 		panic("Could not create events table")
+// 	}
+// }
+
 package db
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sql.DB
 
-func InitDB() {
-	DB, err := sql.Open("sqlite3", "api.db")
-
+func InitDB() error {
+	var err error
+	DB, err = sql.Open("sqlite3", "api.db")
 	if err != nil {
-		panic("Could not connect to database")
+		return fmt.Errorf("could not connect to database: %w", err)
 	}
+	defer DB.Close()
 
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
 
-	createTables()
+	if err := createTables(); err != nil {
+		return fmt.Errorf("could not create tables: %w", err)
+	}
+
+	return nil
 }
 
-func createTables() {
+func createTables() error {
 	createEventsTable := `
-	CREATE TABLE IF NOT EXISTS events (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-		description TEXT NOT NULL,
-		location TEXT NOT NULL,
-		dateTime DATETIME NOT NULL,
-		user_id INTEGER 
-	)
-	`
+        CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            description TEXT NOT NULL,
+            location TEXT NOT NULL,
+            dateTime DATETIME NOT NULL,
+            user_id INTEGER
+        )
+    `
 
 	_, err := DB.Exec(createEventsTable)
-
 	if err != nil {
-		panic("Could not create events table")
+		return fmt.Errorf("could not create events table: %w", err)
 	}
+
+	return nil
 }
--- Rest-Api/main.go
+++ Rest-Api/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/restapi/db"
 	"example.com/restapi/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents) // GET, POST, PUT, DELETE
