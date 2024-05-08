5a85e3fb56a921acc4024763afbd1f3026ceea84
Falasefemi2 <femifalase228@gmail.com>
2024-05-08 23:39:46 +0100

    update event

--- Rest-Api/routes/routes.go
+++ Rest-Api/routes/routes.go
@@ -6,4 +6,5 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)    // GET, POST, PUT, DELETE
 	server.GET("/events/:id", getEvent) // /events/1, /event/4
 	server.POST("/events", createEvent)
+	server.PUT("/events/:id", updateEvent)
 }
