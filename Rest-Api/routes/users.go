c1820f79d1a30e5e9c348570da3702cfb449ec35
Falasefemi2 <femifalase228@gmail.com>
2024-05-11 11:16:17 +0100

    user login and hashed password

--- Rest-Api/routes/users.go
+++ Rest-Api/routes/users.go
@@ -26,3 +26,24 @@ func signUp(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
 }
+
+func login(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the message"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Could not authenticate user."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User logged in successfully"})
+
+}
